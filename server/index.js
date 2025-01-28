9e04e47daef4144b453b1791cad0f820e629cfcf
Karmavir Joshi <karmavirj@live.com>
2025-01-28 09:24:03 +0530

    init express project

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import http from "http";
+
+const app = express();
+dotenv.config();
+const server = http.createServer(app);
+
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Database connected successfully");
+    app.listen(port, () => {
+        console.log("Server started at port ", port);
+    })
+}).catch((error) => {
+    console.log("Error : ", error.message);
+})
+
+app.use(morgan());
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors(
+    {origin:['*'],credentials:true}
+));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+server.listen(port, () => {
+ console.log(`Server is running on port ${port}`);
+});
